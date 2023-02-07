50075b76ae4b04f629b2cd71d723cfc1a8bf8e26
Swapna Gupta <swapna.sony@gmail.com>
2023-02-07 14:21:22 -0800

    change pagination to a sort of infinite scroll with a more link, make default page size for wallet to be 24 nfts

--- app/controllers/wallet_controller.rb
+++ app/controllers/wallet_controller.rb
@@ -5,11 +5,19 @@ class WalletController < ApplicationController
   before_action :authenticate_user
 
   def nfts
-    @page = get_wallet_nfts(current_user.eth_address, 10, wallet_params[:cursor])
+    @page = get_wallet_nfts(current_user.eth_address, 24, wallet_params[:cursor])
     page_number = @page[:page_number]
-    session[:wallet_pages] = {} if page_number == 1
-    session[:wallet_pages][page_number.to_s] = wallet_params[:cursor]
-    @page[:previous_cursor] = page_number > 1 ? session[:wallet_pages][(page_number - 1).to_s] : nil
+
+    if page_number
+      session[:wallet_pages] = {} if page_number == 1
+      session[:wallet_pages][page_number.to_s] = wallet_params[:cursor]
+      @page[:previous_cursor] = page_number > 1 ? session[:wallet_pages][(page_number - 1).to_s] : nil
+    end
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
   end
 
   private
